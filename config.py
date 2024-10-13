cd55b22408f4a4e4752f9c4aabd444674e0b5390
Abhisheka <20535197@student.curtin.edu.au>
2024-10-13 12:02:06 +0530

    mysql -> sqlite

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = 'd3c1f01a3bdf44b8a6c993e752a4f407'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:jkf&&%_f3#DF45@127.0.0.1/expenses_db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'expenses_db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
